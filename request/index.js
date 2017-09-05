4b1c692ae18b468600628daafa9c097d6646ba7a
dreamthen <dreamthen.00@gmail.com>
2017-09-05 23:07:19 +0800

    amazing http request

--- request/index.js
+++ request/index.js
@@ -1,48 +1,51 @@
+// 'Accept': 'application/json, text/javascript, */*; q=0.01',
+//     'Accept-Encoding': 'gzip, deflate',
+//     'Accept-Language': 'zh-CN,zh;q=0.8',
+//     'Connection': 'keep-alive',
+//     'Content-Length': requestData.length,
+//     'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+//     'Cookie': 'imooc_uuid=62413c8f-7af4-4864-83ef-80b23b451ecc; imooc_isnew_ct=1503407895; loginstate=1; apsid=RiZTdlZDgwMjFkMWFhMjA4NzZhZDIzNDAwZDM2YWQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAMjIzNDQ0MQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABkcmVhbXRoZW4uMDBAZ21haWwuY29tAAAAAAAAAAAAAGEzYzZmNTQ4MzBjNjkxZGUzNDE0YzJhNjJmN2MyZGNiKH6cWSh%2BnFk%3DNT; PHPSESSID=blbf4lltkk1e6p9ciimg7l0ff7; IMCDNS=0; Hm_lvt_f0cfcccd7b1393990c78efdeebff3968=1503407897,1504620142; Hm_lpvt_f0cfcccd7b1393990c78efdeebff3968=1504622323; imooc_isnew=2; cvde=59aeae6c3c18c-46',
+//     'Host': 'www.imooc.com',
+//     'Origin': 'http://www.imooc.com',
+//     'Referer': 'http://www.imooc.com/video/8837',
+//     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+//     'X-Requested-With': 'XMLHttpRequest'
 var http = require("http");
 var querystring = require("querystring");
 var requestData = querystring.stringify({
-    user_id: 9355612,
-    resource_id: 1838,
-    user_name: "Mix时代",
-    category: 0,
-    is_additional: 0,
-    resource_title: "深入理解GIT",
-    is_anonymous: 0,
-    lesson_consistent: 5,
-    lecturer_Interpretation: 5,
-    lecturer_answer: 5,
-    content: "学到很多干货"
+    content: "Scott,amazing~",
+    cid: 348
 });
 
 var options = {
-    hostname: "edu.51cto.com",
+    hostname: "www.imooc.com",
     port: 80,
-    path: "/center/appraise/index/add-appraise",
+    path: "/course/docomment",
     method: "POST",
     headers: {
-        Accept: 'application/json, text/javascript, */*; q=0.01',
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
         'Accept-Encoding': 'gzip, deflate',
         'Accept-Language': 'zh-CN,zh;q=0.8',
-        Connection: 'keep-alive',
-        'Content-Length': '302',
-        'Content-Type': 'application/x-www-form-urlencoded',
-        Cookie: 'www51cto=BB109CDE4D5D5EE6AAA40DAF1CF1C9DFybEM; UM_distinctid=15e3bf4a3e2b82-01c1f8e114cf15-791238-1fa400-15e3bf4a3e3ac5; _ourplusFirstTime=117-9-1-13-40-42; EDUACCOUNT=65bc063aa1e79909c7789daebb6afc340d7e9fb5859c9ab6bf3953afd7aabd22a%3A2%3A%7Bi%3A0%3Bs%3A10%3A%22EDUACCOUNT%22%3Bi%3A1%3Bs%3A32%3A%2216361b28cb3a33425f67330788310b52%22%3B%7D; Hm_lvt_110fc9b2e1cae4d110b7959ee4f27e3b=1504244442,1504592306; Hm_lpvt_110fc9b2e1cae4d110b7959ee4f27e3b=1504592306; acw_tc=AQAAANyRSEoSSQwA5B3uc93vbl7O4zw1; sBox=1; pub_sauth1=LA1BrtTRzWsKBwcEVQhUPQUGBFBTCwcCU1I; pub_sauth2=12b5c76c9d8de789878c5991ce1c4189; pub_cookietime=0; PHPSESSID=bit91jal39oifbf1k02h10ni21; _csrf=5571d12000ef6a77e11dfb040c087e791bfbf88ca40c0c26dbffc269dea92271a%3A2%3A%7Bi%3A0%3Bs%3A5%3A%22_csrf%22%3Bi%3A1%3Bs%3A32%3A%223MF7X3yDnEKHAEzp_C_U3SBdwbq6G7Sk%22%3B%7D; bdshare_firstime=1504592362794; 9355612flag=yes; _ourplusReturnCount=15; _ourplusReturnTime=117-9-5-14-19-54; Hm_lvt_8c8abdb71d78d33dfdb885e0bc71dae0=1504244443,1504592307,1504592314; Hm_lpvt_8c8abdb71d78d33dfdb885e0bc71dae0=1504592395; Cto_lvt_=1504244443,1504592307,1504592314; Cto_lpvt_=1504592395',
-        Host: 'edu.51cto.com',
-        Origin: 'http://edu.51cto.com',
-        Referer: 'http://edu.51cto.com/course/1838.html',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+        'Connection': 'keep-alive',
+        'Content-Length': requestData.length,
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Cookie': 'imooc_uuid=62413c8f-7af4-4864-83ef-80b23b451ecc; imooc_isnew_ct=1503407895; loginstate=1; apsid=RiZTdlZDgwMjFkMWFhMjA4NzZhZDIzNDAwZDM2YWQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAMjIzNDQ0MQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABkcmVhbXRoZW4uMDBAZ21haWwuY29tAAAAAAAAAAAAAGEzYzZmNTQ4MzBjNjkxZGUzNDE0YzJhNjJmN2MyZGNiKH6cWSh%2BnFk%3DNT; PHPSESSID=blbf4lltkk1e6p9ciimg7l0ff7; IMCDNS=0; Hm_lvt_f0cfcccd7b1393990c78efdeebff3968=1503407897,1504620142; Hm_lpvt_f0cfcccd7b1393990c78efdeebff3968=1504622323; imooc_isnew=2; cvde=59aeae6c3c18c-46',
+        'Host': 'www.imooc.com',
+        'Origin': 'http://www.imooc.com',
+        'Referer': 'http://www.imooc.com/video/8837',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
         'X-Requested-With': 'XMLHttpRequest'
     }
 };
 
 var req = http.request(options, function (res) {
     console.log("statusCode:" + res.statusCode);
-    console.log("header:" + JSON.stringify(res.headers));
-    res.on("data", function (data) {
-        console.log(JSON.stringify(data));
+    console.log("headers:" + JSON.stringify(res.headers));
+    res.on("data", function (data){
+        console.log(Buffer.isBuffer(data));
         console.log(typeof data);
     });
-    res.on("end", function () {
+    res.on("end", function(){
         console.log("评论完毕~");
     });
 });
@@ -53,7 +56,5 @@ req.on("error", function (e) {
 
 req.write(requestData);
 req.end();
-
-
 
 
